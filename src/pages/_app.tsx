58f02d9447f3e64cfb7e52497163ea666e07f48f
DMeisterling <dan-meisterling@t-online.de>
2023-07-06 00:17:42 +0200

    add profile page withtrpc serverSideHelpers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { type AppType } from "next/app";
+import Head from "next/head";
 import { Toaster } from "react-hot-toast";
 import "~/styles/globals.css";
 import { api } from "~/utils/api";
@@ -7,6 +8,11 @@ import { api } from "~/utils/api";
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Head>
+        <title>Zwitscher</title>
+        <meta name="description" content="Post some Gezwitscher" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Toaster position="bottom-center" />
       <Component {...pageProps} />
     </ClerkProvider>
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,4 +1,3 @@
-import type { User } from "@clerk/nextjs/dist/types/api";
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
@@ -11,14 +10,7 @@ import {
   privateProcedure,
   publicProcedure,
 } from "~/server/api/trpc";
-
-const filterUserForClient = (user: User) => {
-  return {
-    id: user.id,
-    username: user.username,
-    profileImageUrl: user.profileImageUrl,
-  };
-};
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
 // Create a new ratelimiter, that allows 3 requests per 1 minute
 const ratelimit = new Ratelimit({
@@ -74,13 +66,6 @@ export const postsRouter = createTRPCRouter({
     )
     .mutation(async ({ ctx, input }) => {
       const authorId = ctx.userId;
-
-      // if (input.content === "") {
-      //   throw new TRPCError({
-      //     code: "UNPROCESSABLE_CONTENT",
-      //     message: "input cannot be empty!",
-      //   });
-      // }
 
       const { success } = await ratelimit.limit(authorId);
 
